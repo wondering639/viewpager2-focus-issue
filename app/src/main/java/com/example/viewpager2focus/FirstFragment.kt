20d873ef58ec95a48a07593ba0192e39f1d27dc1
wondering639 <25722498+wondering639@users.noreply.github.com>
2020-09-15 15:24:36 +0200

    reproduce error: first tab = no focus, 2nd tab = focus possible. Only difference: android:layout_width="match_parent" vs. 0dp (constraint).

--- app/src/main/java/com/example/viewpager2focus/FirstFragment.kt
+++ app/src/main/java/com/example/viewpager2focus/FirstFragment.kt
@@ -1,6 +1,7 @@
 package com.example.viewpager2focus
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +15,10 @@ class FirstFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
